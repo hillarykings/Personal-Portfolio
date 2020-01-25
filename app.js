666d7bd7b4a2f817b96966f58098ebf445864e22
Hillarykings <hillarypeters23@gmail.com>
2020-01-25 17:28:49 +0100

    updated some css styles

---
+++ app.js
@@ -0,0 +1,11 @@
+ let myNode = document.querySelector('#projects h3')
+
+ myNode.addEventListener('click', function () {
+     console.log('Clicked');
+ })
+// (function() {
+//     //selecting our node 
+   
+   
+
+// })(); //self executing function or immediate invoke  function
